c0d3a4810a227e7be65f13f394ba5dff84d34ebe
inasync <in-async@users.noreply.github.com>
2018-10-29 17:27:21 +0900

    FakeHttpServer を追加。

---
+++ FakeServers/FakeHttpServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Inasync.FakeServers {
+
+    /// <summary>
+    /// <see cref="IHttpServer"/> の Fake 実装。
+    /// </summary>
+    public sealed class FakeHttpServer : HttpServer {
+
+        public static FakeHttpServer StartNew(string url, Action<HttpListenerRequest, HttpListenerResponse> handler) {
+            var httpServer = new FakeHttpServer(url, handler);
+            httpServer.Start();
+            return httpServer;
+        }
+
+        private readonly Action<HttpListenerRequest, HttpListenerResponse> _handler;
+
+        public FakeHttpServer(string url, Action<HttpListenerRequest, HttpListenerResponse> handler) : base(url) {
+            _handler = handler;
+        }
+
+        protected override void OnRequest(HttpListenerContext context) {
+            var request = context.Request;
+            using (var response = context.Response) {
+                _handler?.Invoke(request, response);
+            }
+        }
+
+        protected override void OnException(Exception exception) {
+            Trace.TraceError(exception.ToString());
+        }
+    }
+}
---
+++ FakeServers/HttpServer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Inasync.FakeServers {
+
+    /// <summary>
+    /// HTTP Server を表すインターフェース。
+    /// </summary>
+    public interface IHttpServer : IDisposable {
+
+        /// <summary>
+        /// HTTP Server を起動します。
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// HTTP Server を停止します。
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// HTTP Server を終了します。
+        /// </summary>
+        void Close();
+    }
+
+    /// <summary>
+    /// <see cref="IHttpServer"/> の抽象クラス。
+    /// </summary>
+    public abstract class HttpServer : IHttpServer {
+
+        public static int GetAvailablePort() {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            return port;
+        }
+
+        private readonly HttpListener _listener;
+
+        public HttpServer(string uriPrefix) {
+            if (uriPrefix == null) { throw new ArgumentNullException(nameof(uriPrefix)); }
+
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(uriPrefix);
+        }
+
+        public void Start() {
+            _listener.Start();
+            _listener.BeginGetContext(OnGetContext, null);
+        }
+
+        public void Stop() => _listener.Stop();
+
+        public void Close() => _listener.Close();
+
+        private void OnGetContext(IAsyncResult asyncResult) {
+            try {
+                var context = _listener.EndGetContext(asyncResult);
+                OnRequest(context);
+            }
+            catch (Exception ex) {
+                OnException(ex);
+            }
+
+            if (_listener.IsListening) {
+                _listener.BeginGetContext(OnGetContext, null);
+            }
+        }
+
+        protected abstract void OnRequest(HttpListenerContext context);
+
+        protected abstract void OnException(Exception exception);
+
+        #region IDisposable Support
+
+        private bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing) {
+            if (!_disposed) {
+                if (disposing) {
+                    _listener.Abort();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        #endregion IDisposable Support
+    }
+}
